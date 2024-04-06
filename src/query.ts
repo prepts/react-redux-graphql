cd7cdb2444d719e77c81de77939ca2802f46bb3a
suresh-20698 <imsuresh20698@gmail.com>
2024-04-06 17:11:18 +0530

    used useQuery and useLazyQuery

--- src/query.ts
+++ src/query.ts
@@ -1,10 +1,41 @@
 import { gql } from "@apollo/client";
 
-export const getContriesQuery = gql`
-  query getCountries {
+export const getAllCountriesQuery = gql`
+  query {
     countries {
       name
       code
+    }
+  }
+`;
+
+export const getCountryQuery = gql`
+  query ($countryCode: String) {
+    countries(filter: { code: { eq: $countryCode } }) {
+      name
+      code
+    }
+  }
+`;
+
+export const getAllContinentsQuery = gql`
+  query {
+    continents {
+      name
+      code
+    }
+  }
+`;
+
+export const getAllCountriesInContinentQuery = gql`
+  query ($continentCode: String) {
+    countries(filter: { continent: { eq: $continentCode } }) {
+      name
+      code
+      continent {
+        name
+        code
+      }
     }
   }
 `;
